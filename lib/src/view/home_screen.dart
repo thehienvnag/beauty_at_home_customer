11d800ce65ba8d2574c37f0961f98e537f7d4623
thehienvnag <thehienvnag@gmail.com>
2021-01-26 13:17:09 +0700

    [HienHT] [CR_NamedRoute] Add example home screen

---
+++ lib/src/view/home_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('To Second screen'),
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamed(context, '/second');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('To Home screen'),
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pushNamedAndRemoveUntil(context, "/", (r) => false);
+          },
+        ),
+      ),
+    );
+  }
+}
