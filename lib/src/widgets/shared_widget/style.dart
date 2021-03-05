d06a8ae3d70842ab32db3587c5f1c5027869118c
thehienvnag <thehienvnag@gmail.com>
2021-03-05 23:59:53 +0700

    [HienHT] [Fix_Text_Style] Fix text style

--- lib/src/widgets/shared_widget/style.dart
+++ lib/src/widgets/shared_widget/style.dart
@@ -8,67 +8,69 @@ const SmallTextSize = 12.0;
 
 const String FontNameDefault = 'Montserrat';
 
-const HeaderTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w600,
-  fontSize: MediumTextSize,
-  color: Colors.black,
-);
+final headerTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w600,
+      fontSize: MediumTextSize,
+      color: color,
+    );
 
-const AppBarTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w600,
-  fontSize: MediumTextSize,
-  color: Colors.black,
-);
+final appBarTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w600,
+      fontSize: MediumTextSize,
+      color: Colors.black,
+    );
 
-const HeadlineTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w700,
-  fontSize: LargeTextSize,
-  color: Colors.black,
-);
+final headlineTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w700,
+      fontSize: LargeTextSize,
+      color: Colors.black,
+    );
 
-const TitleTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w500,
-  fontSize: BodyTextSize,
-  color: Colors.black,
-);
+final titleTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w500,
+      fontSize: BodyTextSize,
+      color: Colors.black,
+    );
 
-const SubtitleTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w500,
-  fontSize: SmallTextSize,
-  color: Color(0xFF757575),
-);
+final subtitleTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w500,
+      fontSize: SmallTextSize,
+      color: Color(0xFF757575),
+    );
 
-const Body1TextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w300,
-  fontSize: BodyTextSize,
-  color: Colors.black,
-);
+final body1TextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w300,
+      fontSize: BodyTextSize,
+      color: Colors.black,
+    );
 
-const StatusTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w500,
-  fontSize: TextSizeDefault,
-  color: Color(0xFF0DB5B4),
-);
+final statusTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w500,
+      fontSize: TextSizeDefault,
+      color: Color(0xFF0DB5B4),
+    );
 
-const ButtonTextStyle = TextStyle(
-  fontFamily: FontNameDefault,
-  fontWeight: FontWeight.w500,
-  fontSize: MediumTextSize,
-  color: Colors.white,
-);
+final buttonTextStyle = (Color color) => TextStyle(
+      fontFamily: FontNameDefault,
+      fontWeight: FontWeight.w500,
+      fontSize: MediumTextSize,
+      color: Colors.white,
+    );
 
 class CustomTextStyle {
-  static TextStyle _text900(Color color) => TextStyle(
-    fontFamily: FontNameDefault,
-    fontWeight: FontWeight.w300,
-    fontSize: 27.0,
-    color: color,
-  );
+  static TextStyle _headerText(Color color) => headerTextStyle(color);
+  static TextStyle _appBarText(Color color) => appBarTextStyle(color);
+  static TextStyle _headlineText(Color color) => headlineTextStyle(color);
+  static TextStyle _titleText(Color color) => titleTextStyle(color);
+  static TextStyle _subtitleText(Color color) => subtitleTextStyle(color);
+  static TextStyle _body1Text(Color color) => body1TextStyle(color);
+  static TextStyle _statusTextStyle(Color color) => statusTextStyle(color);
+  static TextStyle _buttonTextStyle(Color color) => buttonTextStyle(color);
 }
