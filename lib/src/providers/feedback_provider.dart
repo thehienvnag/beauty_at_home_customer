18aa806f1a45fa6620547af5e9b57e22368993b3
thehienvnag <thehienvnag@gmail.com>
2021-03-23 03:31:09 +0700

    [HienHT] Add missing files

---
+++ lib/src/providers/feedback_provider.dart
@@ -0,0 +1,19 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/src/apis/provider_api/simple_api.dart';
+import 'package:flutter_app/src/models-new/feedback_model.dart';
+import 'package:flutter_app/src/utils/api_constants.dart';
+
+class FeedbackProvider extends ChangeNotifier {
+  List<FeedbackModel> _listFeedback;
+  List<FeedbackModel> get listFeedback => _listFeedback;
+
+  void initListFeedbackByServiceId(String id) async {
+    _listFeedback = await SimpleAPI.getAll<FeedbackModel>(
+      EntityEndpoint.FEEDBACK,
+      fromJson: (json) => FeedbackModel.fromJson(json),
+    );
+    notifyListeners();
+  }
+}
